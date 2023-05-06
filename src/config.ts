16a91351f22accad51f71e2be07b04f3671d49ec
basuda <66863247+basuda@users.noreply.github.com>
2023-05-06 13:20:01 +0530

    minor

--- src/config.ts
+++ src/config.ts
@@ -39,7 +39,7 @@ export const Prompt =
  Finally, provide 3-5 suggestions for related topics or future research directions \
  based on the content of the paper.\
  If applicable, list at least 5 relevant references from the field of study of the paper.\
- Here is the paper. If the last sentence provided is incomplete just ignore if for summarizing :\
+ Here is the paper. If the last sentence provided is incomplete just ignore it for summarizing :\
  '
 
 export interface SitePrompt {
